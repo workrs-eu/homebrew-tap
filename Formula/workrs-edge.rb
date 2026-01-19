851eec969241f11a0bc9809b26843274436fc90e
GitLab CI <ci@workrs.eu>
2026-01-19 14:10:30 +0000

    Update workrs-edge to 0.1.6

--- Formula/workrs-edge.rb
+++ Formula/workrs-edge.rb
@@ -1,28 +1,28 @@
 class WorkrsEdge < Formula
   desc "EU Edge Platform CLI - Build and deploy workers"
   homepage "https://workrs.eu"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
 
   on_macos do
     on_intel do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-x86_64-apple-darwin.tar.gz"
-sha256 "83d50f7456780f2af8fe4f079793d0700df192cc6a79a091c0291eee0eec9f2e"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.6/workrs-edge-x86_64-apple-darwin.tar.gz"
+sha256 "6d2ea1ab1dde8651efb5015b460acac8bab5f12c8b92c12b0dcdb19d99410e7d"
     end
     on_arm do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-aarch64-apple-darwin.tar.gz"
-sha256 "a6c9c4554ba24c31581e2fd68fc830e2e732a30ae0e7388531dad4cff8437e18"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.6/workrs-edge-aarch64-apple-darwin.tar.gz"
+sha256 "a8b907e8b020bb3662ed920f83199d7701f9e81ff9a8c1ef3d881634250f0a88"
     end
   end
 
   on_linux do
     on_intel do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-x86_64-unknown-linux-gnu.tar.gz"
-sha256 "c22f3e68bc9abb0ee37228d7c663320955e0a5f48c78c8c0e8292159036c10f8"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.6/workrs-edge-x86_64-unknown-linux-gnu.tar.gz"
+sha256 "cd27c05b708c092366517423577a86860a3075f7129b17de6122410d7a0a72f5"
     end
     on_arm do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-aarch64-unknown-linux-gnu.tar.gz"
-sha256 "376fe072c50c7a0f892379b23ee3d849d81b91dada6d0cb8db22f70a4d7609a1"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.6/workrs-edge-aarch64-unknown-linux-gnu.tar.gz"
+sha256 "81f58f0d0c596469495d477d5ad7e91d6f8aafe21fe44097da94b00623610f05"
     end
   end
 
