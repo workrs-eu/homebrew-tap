f44908b70f435f76420351cac52f9592b7a432fc
GitLab CI <ci@workrs.eu>
2026-01-19 13:00:42 +0000

    Update workrs-edge to 0.1.5

--- Formula/workrs-edge.rb
+++ Formula/workrs-edge.rb
@@ -1,36 +1,36 @@
 class WorkrsEdge < Formula
   desc "EU Edge Platform CLI - Build and deploy workers"
   homepage "https://workrs.eu"
-  version "0.1.3"
+  version "0.1.5"
   license "MIT"
 
   on_macos do
     on_intel do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.3/edge-x86_64-apple-darwin.tar.gz"
-sha256 "3cf26752aef3585be86b7114ce4549267a4b549a15e4c780aca3d0878800795a"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-x86_64-apple-darwin.tar.gz"
+sha256 "83d50f7456780f2af8fe4f079793d0700df192cc6a79a091c0291eee0eec9f2e"
     end
     on_arm do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.3/edge-aarch64-apple-darwin.tar.gz"
-sha256 "f09a45770e16497ec9729411e405a73f1637340740cbb07f1a534c3733e291e4"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-aarch64-apple-darwin.tar.gz"
+sha256 "a6c9c4554ba24c31581e2fd68fc830e2e732a30ae0e7388531dad4cff8437e18"
     end
   end
 
   on_linux do
     on_intel do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.3/edge-x86_64-unknown-linux-gnu.tar.gz"
-sha256 "703f08b9b878a25379354cb52d07275a125e29ad3b7dcebe6b1d481b99c3fff6"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-x86_64-unknown-linux-gnu.tar.gz"
+sha256 "c22f3e68bc9abb0ee37228d7c663320955e0a5f48c78c8c0e8292159036c10f8"
     end
     on_arm do
-url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.3/edge-aarch64-unknown-linux-gnu.tar.gz"
-sha256 "885da09a099db4045d29d95fcaf497882cb0b87a5d992040476dac0353700901"
+url "https://github.com/workrs-eu/homebrew-tap/releases/download/v0.1.5/workrs-edge-aarch64-unknown-linux-gnu.tar.gz"
+sha256 "376fe072c50c7a0f892379b23ee3d849d81b91dada6d0cb8db22f70a4d7609a1"
     end
   end
 
   def install
-    bin.install "edge"
+    bin.install "workrs-edge"
   end
 
   test do
-    assert_match version.to_s, shell_output("\#{bin}/edge --version")
+    assert_match version.to_s, shell_output("\#{bin}/workrs-edge --version")
   end
 end
